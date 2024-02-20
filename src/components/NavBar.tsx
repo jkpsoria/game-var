dc6349b7c5213b185a47a1cbd27a137519bade04
jkpsoria <90975217+jkpsoria@users.noreply.github.com>
2024-02-20 13:55:51 +0000

    practice update with codespaces

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,25 @@
 import { HStack, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import logo from "../assets/gamevar.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import "../App.css"; // Import CSS file
+
 
 interface Props {
   onSearch: (searchText: string) => void;
 }
 
 const NavBar = ({ onSearch }: Props) => {
+  const handleClick = () => {
+    window.location.href = "/";
+  };
   return (
     <HStack padding="20px">
-      <Image src={logo} boxSize="50px" borderRadius="10px" margin="" />
+      {/* Wrap Image with a Link or a div/button */}
+      <div onClick={handleClick} className="pointerImage">
+        <Image src={logo} boxSize="50px" borderRadius="10px" margin="" />
+      </div>
       <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
